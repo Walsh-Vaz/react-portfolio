0845058be975baa20a3e2aef9b2b88c756154bc7
Walsh Vaz <walshivaz@yahoo.com>
2023-06-27 13:01:34 -0400

    updated the aboutMe.js

---
+++ components/pages/aboutMe.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const styles = {
+    card : {
+        margin: 40,
+        background: "yellow",
+    },
+
+    header: {
+        background: "blue",
+        minHeight: 50,
+        lineHeight: 1.3,
+        fontSize: "1.2rem",
+        color: "white",
+        padding: "0 20px",
+    },
+
+    content: {
+        background: "green",
+        color: "white",
+        padding: 20,
+    },
+};
+
+function aboutMe() {
+
+    retrun (
+        <div style={styles.card}> 
+            <h1 style={styles.header}> About Me</h1>
+            <p style={styles.content}>Hello Everyone, My name is Walsh Vaz. I am currently studying web development at the
+               Carleton University bootcamp. I have done my undergrad in Computer Science at Carleton 
+               University and Im currently doing this bootcamp to upgrade myself and land a job in my
+               field.  
+            </p>
+        </div>
+    );
+}
+
+export default aboutMe;

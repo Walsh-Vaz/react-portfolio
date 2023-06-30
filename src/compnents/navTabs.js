3541f49b29c3cad500366e690c3111b8da3a1027
Walsh Vaz <walshivaz@yahoo.com>
2023-06-29 20:13:51 -0400

    updated the navtabs

--- src/compnents/navTabs.js
+++ src/compnents/navTabs.js
@@ -41,6 +41,17 @@ function NavTabs({ currentPage, handlePageChange }) {
 
           className={currentPage === 'aboutMe' ? 'nav-link active' : 'nav-link'}
         >
+          Walsh Vaz
+        </a>
+      </li>
+      <li style={styles.li}>
+        <a
+          href="#aboutMe"
+          onClick={() => handlePageChange('aboutMe')}
+          
+
+          className={currentPage === 'aboutMe' ? 'nav-link active' : 'nav-link'}
+        >
           About Me
         </a>
       </li>
@@ -64,6 +75,18 @@ function NavTabs({ currentPage, handlePageChange }) {
           className={currentPage === 'contactInfo' ? 'nav-link active' : 'nav-link'}
         >
           Contact Info
+        </a>
+      </li>
+
+      <li style={styles.li}>
+        <a
+          href="https://onedrive.live.com/edit.aspx?resid=24A26146364404F2!4588&ithint=file%2cdocx&ct=1683838261012&wdOrigin=OFFICECOM-WEB.MAIN.EDGEWORTH&wdPreviousSessionSrc=HarmonyWeb&wdPreviousSession=f00c3118-e81a-4254-9569-844f5d4db87d"
+          
+
+          //onClick={() => handlePageChange('contactInfo')}
+          //className={currentPage === 'contactInfo' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
         </a>
       </li>
     </ul>
